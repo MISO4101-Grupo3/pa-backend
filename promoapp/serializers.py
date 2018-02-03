7f0df8b6c48b7afc669e447b81bf59c753c203ef
Samuel Salazar <ss.salazar10@uniandes.edu.co>
2018-02-03 17:06:48 -0500

    Dejar todos los campos en los serializadores

--- promoapp/serializers.py
+++ promoapp/serializers.py
@@ -9,14 +9,14 @@ from .models import *
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
-        fields= ('id','nombre',)
+        #fields= ('id','nombre',)
         read_only_fields= ('id',)
 
 # Ciudad
 class CiudadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ciudad
-        fields= ('id', 'nombre',)
+        #fields= ('id', 'nombre',)
         read_only_fields= ('id',)
 
 
@@ -24,14 +24,14 @@ class CiudadSerializer(serializers.ModelSerializer):
 class PromocionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Promocion
-        fields= ('id','nombre','descripcion','comentarios',)
+        #fields= ('id','nombre','descripcion','comentarios',)
         read_only_fields= ('id',)
 
 # Comentario
 class ComentarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comentario
-        fields= ('id','texto','correo','promocion')
+        #fields= ('id','texto','correo','promocion')
 
         read_only_fields = ('id',)
 
